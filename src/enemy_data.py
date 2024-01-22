50c193ffb80c5d5999ae579d88bcaa030f6e1929
dtzampanakis <dimitris.tzampanakis@gugroup.com>
2024-01-22 09:26:42 +0200

    first commit

---
+++ src/enemy_data.py
@@ -0,0 +1,228 @@
+ENEMY_SPAWN_DATA = [
+  {
+    #1
+    "weak": 15,
+    "medium": 0,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #2
+    "weak": 30,
+    "medium": 0,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #3
+    "weak": 20,
+    "medium": 5,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #4
+    "weak": 30,
+    "medium": 15,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #5
+    "weak": 5,
+    "medium": 20,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #6
+    "weak": 15,
+    "medium": 15,
+    "strong": 4,
+    "elite": 0
+  },
+  {
+    #7
+    "weak": 20,
+    "medium": 25,
+    "strong": 5,
+    "elite": 0
+  },
+  {
+    #8
+    "weak": 10,
+    "medium": 20,
+    "strong": 15,
+    "elite": 0
+  },
+  {
+    #9
+    "weak": 15,
+    "medium": 10,
+    "strong": 5,
+    "elite": 0
+  },
+  {
+    #10
+    "weak": 0,
+    "medium": 100,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #11
+    "weak": 5,
+    "medium": 10,
+    "strong": 12,
+    "elite": 2
+  },
+  {
+    #12
+    "weak": 0,
+    "medium": 15,
+    "strong": 10,
+    "elite": 5
+  },
+  {
+    #13
+    "weak": 20,
+    "medium": 0,
+    "strong": 25,
+    "elite": 10
+  },
+  {
+    #14
+    "weak": 15,
+    "medium": 15,
+    "strong": 15,
+    "boss": 0,
+    "elite": 15
+  },
+  {
+    #15
+    "weak": 0 ,
+    "medium": 0,
+    "strong": 0,
+    "boss": 1,
+    "elite":0 
+  },
+  {
+    #16
+    "weak": 15,
+    "medium": 15,
+    "strong": 15,
+    "boss": 0,
+    "elite": 15
+  },
+  {
+    #17
+    "weak": 15,
+    "medium": 15,
+    "strong": 15,
+    "boss": 0,
+    "elite": 15
+  },
+  {
+    #18
+    "weak": 15,
+    "medium": 15,
+    "strong": 15,
+    "boss": 0,
+    "elite": 15
+  },
+  {
+    #19
+    "weak": 15,
+    "medium": 15,
+    "strong": 15,
+    "boss": 0,
+    "elite": 15
+  },
+  {
+    #20
+    "weak": 15,
+    "medium": 15,
+    "strong": 15,
+    "boss": 0,
+    "elite": 15
+  },
+  {
+    #21
+    "weak": 15,
+    "medium": 15,
+    "strong": 15,
+    "boss": 0,
+    "elite": 15
+  },
+  {
+    # 22
+    "weak": 15,
+    "medium": 15,
+    "strong": 15,
+    "boss": 0,
+    "elite": 15
+  },
+  {
+    # 23
+    "weak": 15,
+    "medium": 15,
+    "strong": 15,
+    "boss": 1,
+    "elite": 15
+  },
+  {
+    # 24
+    "weak": 15,
+    "medium": 15,
+    "strong": 15,
+    "boss": 1,
+    "elite": 15
+  },
+  {
+    # 25
+    "weak": 15,
+    "medium": 15,
+    "strong": 15,
+    "boss": 2,
+    "elite": 15
+  },
+  {
+    # 26
+    "weak": 25,
+    "medium": 25,
+    "strong": 25,
+    "boss": 3,
+    "elite": 25
+  },
+  {
+    # 27
+    "weak": 25,
+    "medium": 25,
+    "strong": 25,
+    "boss": 23,
+    "elite": 25
+  }
+]
+
+ENEMY_DATA = {
+    "weak": {
+    "health": 10,
+    "speed": 2
+  },
+    "medium": {
+    "health": 15,
+    "speed": 3
+  },
+    "strong": {
+    "health": 20,
+    "speed": 4
+  },
+    "boss": {
+    "health": 350,
+    "speed": 2
+  },
+    "elite": {
+    "health": 30,
+    "speed": 6
+  }
+}
